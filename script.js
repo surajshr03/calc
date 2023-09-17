b6fd1465775ffd25cc060a99a6350997dab4bed4
surajshr03 <suraj.shr03@gmail.com>
2023-09-17 08:09:03 +0545

    js remaining commit

--- script.js
+++ script.js
@@ -9,6 +9,15 @@ arr.forEach(button=>
             if(e.target.innerHTML=="="){
                   string=eval(string);
                   input.value=string;
+                  string="";
+            }
+            else if(e.target.innerHTML=="AC"){
+                  string="";
+                  input.value=string;
+            }
+            else if(e.target.innerHTML=="DEL"){
+                  string=string.substring(0,string.length-1);
+                  input.value=string
             }
 
 
